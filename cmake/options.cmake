0956aeb8b6b9d40f49d996e2db4b4defc90d8461
Cantte <cantte098@gmail.com>
2021-04-27 08:19:53 -0500

    feat: Add cmake configs

---
+++ cmake/options.cmake
@@ -0,0 +1,3 @@
+
+set(WITH_SOURCE_TREE "hierarchical" CACHE STRING "Build the source tree for IDE's.")
+set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical hierarchical-folders)
