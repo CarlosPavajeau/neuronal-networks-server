9acfc726a9f08b7dd03c628d5f8ea44e5569386e
Cantte <cantte098@gmail.com>
2021-04-28 10:55:18 -0500

    feat: Add random utilities

---
+++ src/common/utilities/Random.cpp
@@ -0,0 +1,15 @@
+//
+// Created by cantte on 28/04/21.
+//
+
+#include "Random.h"
+
+#include <random>
+
+static std::mt19937 engine;
+
+double drand(double min, double max)
+{
+    std::uniform_real_distribution<double> uid(min, max);
+    return uid(engine);
+};
---
+++ src/common/utilities/Random.h
@@ -0,0 +1,11 @@
+//
+// Created by cantte on 28/04/21.
+//
+
+#ifndef NEURONAL_NETWORKS_SERVER_RANDOM_H
+#define NEURONAL_NETWORKS_SERVER_RANDOM_H
+
+/* Return a random number in the range min..max */
+double drand(double min, double max);
+
+#endif //NEURONAL_NETWORKS_SERVER_RANDOM_H
