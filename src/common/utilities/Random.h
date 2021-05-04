74397a1c14ecf95d15ec766a690d7cc99abb662b
Cantte <cantte098@gmail.com>
2021-05-04 15:17:25 -0500

    feat: Add random generator

--- src/common/utilities/Random.h
+++ src/common/utilities/Random.h
@@ -8,4 +8,13 @@
 /* Return a random number in the range min..max */
 double drand(double min, double max);
 
+struct gen_rand
+{
+public:
+    double operator()()
+    {
+        return drand(-1, 1);
+    }
+};
+
 #endif //NEURONAL_NETWORKS_SERVER_RANDOM_H
