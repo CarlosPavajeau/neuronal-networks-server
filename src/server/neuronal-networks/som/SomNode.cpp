f9a50176bbf1375845cc6cfcfbe61c0f4212cd8d
Cantte <cantte098@gmail.com>
2021-05-05 11:33:16 -0500

    feat: Add SomNode

---
+++ src/server/neuronal-networks/som/SomNode.cpp
@@ -0,0 +1,47 @@
+//
+// Created by cantte on 4/05/21.
+//
+
+#include "SomNode.h"
+#include <cassert>
+
+SomNode::SomNode(const Point& coordinate, int num_inputs) : Node(num_inputs)
+{
+    _coordinate = coordinate;
+}
+
+SomNode::SomNode(uint64 x, uint64 y, int num_inputs) : Node(num_inputs)
+{
+    _coordinate = Point(x, y);
+}
+
+SomNode::SomNode()
+{
+    _coordinate = Point(0, 0);
+}
+
+void SomNode::SetCoordinate(uint64 x, uint64 y)
+{
+    _coordinate = Point(x, y);
+}
+
+double SomNode::GetEuclideanDistance(std::vector<double> inputs)
+{
+    assert(inputs.size() == _weights.size());
+
+    double distance = 0.0;
+    for (size_t i = 0; i < _weights.size(); ++i)
+    {
+        distance += std::pow((inputs[i] - _weights[i]), 2);
+    }
+
+    return std::sqrt(distance);
+}
+
+void SomNode::UpdateWeights(double learning_rate, double winner_distance)
+{
+    for (double& _weight : _weights)
+    {
+        _weight += learning_rate * winner_distance;
+    }
+}
---
+++ src/server/neuronal-networks/som/SomNode.h
@@ -0,0 +1,44 @@
+//
+// Created by cantte on 4/05/21.
+//
+
+#ifndef NEURONAL_NETWORKS_SERVER_SOMNODE_H
+#define NEURONAL_NETWORKS_SERVER_SOMNODE_H
+
+#include "Node.h"
+#include "Utils.h"
+
+class SomNode : public Node
+{
+public:
+    explicit SomNode(const Point& coordinate, int num_inputs);
+
+    SomNode(uint64 x, uint64 y, int num_inputs);
+
+    SomNode();
+
+    const Point& GetCoordinate() const { return _coordinate; }
+
+    void SetCoordinate(const Point& new_coordinate) { _coordinate = new_coordinate; }
+
+    void SetCoordinate(uint64 x, uint64 y);
+
+    uint64 GetX() const { return _coordinate.X; }
+
+    uint64 GetY() const { return _coordinate.Y; }
+
+    void SetX(uint64 new_x) { _coordinate.X = new_x; }
+
+    void SetY(uint64 new_y) { _coordinate.X = new_y; }
+
+    double GetEuclideanDistance(std::vector<double> inputs);
+
+    void UpdateWeights(double learning_rate, double winner_distance);
+
+protected:
+    Point _coordinate;
+    std::vector<float> _distance_to;
+};
+
+
+#endif //NEURONAL_NETWORKS_SERVER_SOMNODE_H
