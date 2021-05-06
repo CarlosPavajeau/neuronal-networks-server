9a88062ba17c1cbb04fd1d7c66f83d1f0d4db2d4
Cantte <cantte098@gmail.com>
2021-05-06 16:07:58 -0500

    feat(som-parsers): Fix some parse bugs

--- src/server/models/parsers/SelfOrgMapParsers.cpp
+++ src/server/models/parsers/SelfOrgMapParsers.cpp
@@ -13,10 +13,19 @@ namespace json = boost::json;
 SelfOrgMapCreateInfo
 tag_invoke(const boost::json::value_to_tag<SelfOrgMapCreateInfo>&, const boost::json::value& value)
 {
-    uint64 width = value.at("Width").as_uint64();
-    uint64 height = value.at("Height").as_uint64();
-    int num_inputs = value.at("InputsNumber").as_uint64();
-    double delta = value.at("Delta").as_double();
+    uint64 width = value.at("Width").as_int64();
+    uint64 height = value.at("Height").as_int64();
+    int num_inputs = (int) value.at("InputsNumber").as_int64();
+
+    double delta;
+    if (value.at("Delta").is_double())
+    {
+        delta = value.at("Delta").as_double();
+    } else
+    {
+        delta = (double) value.at("Delta").as_int64();
+    }
+
     bool tough_competition = value.at("ToughCompetition").as_bool();
 
     return {width, height, num_inputs, delta, tough_competition};
@@ -27,7 +36,7 @@ tag_invoke(const boost::json::value_to_tag<SelfOrgMapTrainingInfo>&, const boost
 {
     double error_tolerance = value.at("ErrorTolerance").as_double();
     double learning_rate = value.at("LearningRate").as_double();
-    int max_steps = value.at("MaxSteps").as_int64();
+    int max_steps = (int) value.at("MaxSteps").as_int64();
     auto inputs = json::value_to<std::vector<std::vector<double>>>(value.at("Inputs"));
 
     return SelfOrgMapTrainingInfo(inputs, error_tolerance, learning_rate, max_steps);
