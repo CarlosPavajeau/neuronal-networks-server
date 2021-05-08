8a69c1097d0fae132725defebfb65acde23a7d70
Cantte <cantte098@gmail.com>
2021-05-08 11:00:27 -0500

    feat(utils): Add new utility functions

--- src/common/utilities/Utils.h
+++ src/common/utilities/Utils.h
@@ -12,6 +12,7 @@
 #include <functional>
 #include <unordered_map>
 #include <algorithm>
+#include <Eigen/Dense>
 
 inline double sigmoid(double x)
 {
@@ -41,6 +42,16 @@ inline double linear(double x)
 inline double d_linear(double x)
 {
     return 1;
+}
+
+inline double thin_plate_spline(double x)
+{
+    return std::pow(x, 2) * std::log(x);
+}
+
+inline double d_thin_plate_spline(double x)
+{
+    return 2 * x * std::log(x) + x;
 }
 
 struct Point
@@ -81,6 +92,7 @@ private:
         AddNewPair("sigmoid", sigmoid, d_sigmoid);
         AddNewPair("tanh", hyperbolic_tan, d_hyperbolic_tan);
         AddNewPair("linear", linear, d_linear);
+        AddNewPair("thin_plate_spline", thin_plate_spline, d_thin_plate_spline);
     }
 
     void AddNewPair(std::string function_name, std::function<double(double)> function,
@@ -92,6 +104,8 @@ private:
     std::unordered_map<std::string, std::pair<std::function<double(double)>, std::function<double(
             double)>>> activation_functions_map;
 };
+
+#define sActivationFunctionMgr ActivationFunctionsMgr::instance()
 
 inline void Softmax(std::vector<double>* output)
 {
@@ -116,6 +130,26 @@ inline void GetIdMaxElement(const std::vector<double>& output, size_t* class_id)
     *class_id = std::distance(output.begin(), std::max_element(output.begin(), output.end()));
 }
 
-#define sActivationFunctionMgr ActivationFunctionsMgr::instance()
+inline void matrix_vector_to_eigen_matrix(const std::vector<std::vector<double>>& x, Eigen::MatrixXd* matrix)
+{
+    matrix->resize(x.size(), x[0].size());
+
+    for (int i = 0; i < x.size(); ++i)
+    {
+        for (int j = 0; j < x[i].size(); ++j)
+        {
+            (*matrix)(i, j) = x[i][j];
+        }
+    }
+}
+
+inline void vector_to_eigen_vector(const std::vector<double>& x, Eigen::VectorXd* vector)
+{
+    vector->resize(x.size());
+    for (int i = 0; i < x.size(); ++i)
+    {
+        (*vector)[i] = x[i];
+    }
+}
 
 #endif //NEURONAL_NETWORKS_SERVER_UTILS_H
