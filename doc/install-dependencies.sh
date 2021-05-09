56a2a56e356019dc3ff506837c7aa7ebca6d9ec5
Cantte <cantte098@gmail.com>
2021-05-09 18:33:39 -0500

    build: Fix the dependency installation script

--- doc/install-dependencies.sh
+++ doc/install-dependencies.sh
@@ -3,12 +3,12 @@
 # Install all project dependencies
 #
 
-apt-get install -y cmake wget
+apt-get install -y cmake
 
 # install Eigen
-curl -L https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz | tar -zxf -
-cd eigen-3.3.9 && mkdir "build" && cd build && cmake ../ && make install && cd ..
+cd /usr/ && curl -L https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz | tar -zxf -
+cd eigen-3.3.9 && mkdir "build" && cd build && cmake ../ && make install
 
 # install Boost libraries
-wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2 | tar -zxf -
-cd boost_1_76_0 && ./boostrap.sh --prefix=/usr/ && ./b2 && ./b2 install && cd ..
+cd /usr/ && curl -L https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz | tar -zxf -
+cd boost_1_76_0 && ./bootstrap.sh --prefix=/usr/ && ./b2 && ./b2 install
